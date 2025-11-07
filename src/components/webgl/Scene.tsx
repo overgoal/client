d056f7ffc90c591d0f3057c043ff461de544305b
BertovDev <bertobautista18@gmail.com>
2025-11-07 00:18:49 -0300

    fix

--- src/components/webgl/Scene.tsx
+++ src/components/webgl/Scene.tsx
@@ -2,9 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload } from "@react-three/drei";
 import { Suspense, useMemo } from "react";
 import Lights from "./components/lights";
-import { MaleBody1 } from "../../components/models/MaleLast";
 import { ModelBody2 } from "../../components/models/MaleBody2";
-import { ModelBody3 } from "../../components/models/MaleBody3";
 
 const Scene = () => {
   const isMobile = useMemo(
