9c333e4956d1bdd750287453f2cfe3212531b218
BertovDev <bertobautista18@gmail.com>
2025-11-11 14:46:51 -0300

    New screen

--- src/app/(main)/card/ClaimScreen.tsx
+++ src/app/(main)/card/ClaimScreen.tsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import ClaimScene from "../claim/ClaimScene";
 import LoginButton from "../../../components/common/LoginButton";
-import CyberContainer from "../Home/components/cyber-container";
 import LoadingScreen from "../../../components/loader/LoadingScreen";
+
 
 const ClaimScreen = () => {
   const { id } = useParams();
@@ -24,17 +24,18 @@ const ClaimScreen = () => {
       }, 300);
       return () => clearTimeout(timer);
     } else {
-      const progress = Object.values(assetsLoaded).filter(Boolean).length / 2 * 100;
+      const progress =
+        (Object.values(assetsLoaded).filter(Boolean).length / 2) * 100;
       setLoadingProgress(progress);
     }
   }, [assetsLoaded]);
 
   const handleBackgroundLoad = () => {
-    setAssetsLoaded(prev => ({ ...prev, background: true }));
+    setAssetsLoaded((prev) => ({ ...prev, background: true }));
   };
 
   const handleSceneLoadComplete = () => {
-    setAssetsLoaded(prev => ({ ...prev, scene: true }));
+    setAssetsLoaded((prev) => ({ ...prev, scene: true }));
   };
 
   return (
@@ -43,19 +44,22 @@ const ClaimScreen = () => {
       <LoadingScreen isLoading={isLoading} progress={loadingProgress} />
 
       <img
-        src="/backgrounds/bg-card.webp"
+        src="/claim/claims-bg.webp"
         alt="background"
         className="absolute inset-0 z-0 h-full w-full object-cover"
         onLoad={handleBackgroundLoad}
       />
-      <div className="absolute inset-0 top-[90%] left-1/2 z-90 flex h-1/4 w-full -translate-x-1/2 -translate-y-1/2 items-center justify-center bg-linear-to-b from-transparent to-black">
-        <CyberContainer className="airstrike-normal flex h-20 w-[80%] items-center justify-center bg-transparent text-center text-5xl font-bold text-white">
-          <LoginButton className="airstrike-normal flex text-5xl font-bold text-white hover:scale-105 hover:bg-transparent">
-            <span className="airstrike-normal text-4xl">Claim</span>
+      <div className="absolute inset-0 top-[90%] left-1/2 z-90 flex h-1/4 w-full -translate-x-1/2 -translate-y-1/2 items-center justify-center">
+        <div className="h-24 w-full bg-[url('/common/button-container.webp')] bg-contain bg-no-repeat bg-center flex items-center justify-center">
+          <LoginButton className="airstrike-normal flex  font-bold text-white hover:scale-105 hover:bg-transparent">
+            <span className="airstrike-normal text-5xl">Claim</span>
           </LoginButton>
-        </CyberContainer>
+        </div>
       </div>
-      <ClaimScene playerLinkId={id || ""} onLoadComplete={handleSceneLoadComplete} />
+      <ClaimScene
+        playerLinkId={id || ""}
+        onLoadComplete={handleSceneLoadComplete}
+      />
     </div>
   );
 };
